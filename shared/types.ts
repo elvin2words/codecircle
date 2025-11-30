133022dbce82d92958b5775276524de627128e8e
Elvin Mazwimairi <eemazwi2@gmail.com>
2025-11-30 21:14:50 +0200

    added a types.ts to use for vercel client side rendering inplace of SBR which called schema file

---
+++ shared/types.ts
@@ -0,0 +1,11 @@
+// shared/types.ts
+import { z } from "zod";
+
+export const contactFormSchema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  subject: z.string().min(3),
+  message: z.string().min(10),
+});
+
+export type InsertContact = z.infer<typeof contactFormSchema>;
